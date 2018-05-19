f31c0896063e0123eeb6e8574ea86a37bf8bf088
RK224 <rahulkhapre1998@gmail.com>
2018-05-19 17:57:10 +0530

    RSS feed for techCrunch

---
+++ techCrunch/spiders/techcrunch.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class TechcrunchSpider(scrapy.Spider):
+    name = "techcrunch"
+    allowed_domains = ["techcrunch.com/feed/"]
+    start_urls = (
+        'http://www.techcrunch.com/feed//',
+    )
+
+    def parse(self, response):
+        pass
